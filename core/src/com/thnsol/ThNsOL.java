cd270d3b60a612be787625fc858f17f6ae54c068
drzzm32 <drzzm32@gmail.com>
2015-01-07 18:05:07 +0800

    Resources change, Icon add, view type build.

--- core/src/com/thnsol/ThNsOL.java
+++ core/src/com/thnsol/ThNsOL.java
@@ -3,80 +3,78 @@ package com.thnsol;
 import com.badlogic.gdx.ApplicationAdapter;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.utils.*;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.environment.*;
 import com.badlogic.gdx.graphics.g3d.loader.*;
+import com.badlogic.gdx.graphics.g3d.model.data.*;
 import com.badlogic.gdx.graphics.g3d.utils.*;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import com.thnsol.CharacterCameraInputController;
+
 public class ThNsOL extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	ObjLoader loader;
-	FileHandle hDC;
 	Model mod, board;
-	ModelInstance modins, boardins;
+	ModelInstance modins, boardins, tenshiins, landins;
 	ModelBatch modbatch;
 	PerspectiveCamera camera;
 	ModelBuilder modelBuilder;
 	Environment environment;
-	CameraInputController camctrl;
+	CharacterCameraInputController camctrl;
+
+	float mx = 0f, my = 10f, mz = 10f;
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
 		modbatch = new ModelBatch();
 
-		img = new Texture("badlogic.jpg");
-		//hDC = Gdx.files.internal("board.obj");
-		loader = new ObjLoader();
-		//board = loader.loadModel(hDC);
-
 		// ////////LIGHT//////////
 		environment = new Environment();
-		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f,
-				0.4f, 0.4f, 1f));
-		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f,
-				-0.8f, -0.2f));
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.5f,
+				0.5f, 0.5f, 1f));
+		environment.add(new DirectionalLight().set(0.5f, 0.5f, 0.5f, -1f, -1f,
+				-1f));
 
 		// ////////MODEL BUILD//////////
 		modelBuilder = new ModelBuilder();
 		mod = modelBuilder.createBox(5f, 5f, 5f,
 				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
 				Usage.Position | Usage.Normal);
-		board = modelBuilder.createLineGrid(50, 50, 1f, 1f, new Material(
+		modins = new ModelInstance(mod);
+
+		board = modelBuilder.createLineGrid(500, 500, 5f, 5f, new Material(
 				ColorAttribute.createDiffuse(Color.CYAN)), Usage.Position
 				| Usage.Normal);
-
-		// ////////MODEL CONVERT//////////
-		modins = new ModelInstance(mod);
-		modins.transform = new Matrix4(new Vector3(0f, 10f, 0f),
-				new Quaternion(0f, 0f, 0f, 0f), new Vector3(1f, 1f, 1f));
 		boardins = new ModelInstance(board);
 
+		// ////////MODEL CONVERT//////////
+		modins.transform.setToTranslation(0f, 10f, 0f);
+		
 		// ////////CAMERA//////////
 		camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(),
 				Gdx.graphics.getHeight());
 		camera.near = 0.1f;
-		camera.far = 300f;
-		camera.position.set(10f, 10f, 10f);
-		camera.lookAt(0, 0, 0);
+		camera.far = 1000f;
+		camera.position.set(mx + 10f, my + 10f, mz);
+		camera.lookAt(mx, my, mz);
 		camera.update();
 
 		// ////////CONTROL//////////
-		camctrl = new CameraInputController(camera);
+		camctrl = new CharacterCameraInputController(camera);
+		camctrl.target = camera.position;
 		Gdx.input.setInputProcessor(camctrl);
-
 	}
 
 	@Override
@@ -86,23 +84,52 @@ public class ThNsOL extends ApplicationAdapter {
 				Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-		batch.begin();
-		batch.draw(img, 0f, 0f);
-		batch.end();
-
 		modbatch.begin(camera);
 		modbatch.render(modins, environment);
-		img.bind();
 		modbatch.render(boardins, environment);
 		modbatch.end();
 
+		ModelUpdate();
+		ControlUpdate();
 		camctrl.update();
 	}
+	
+	private ModelInstance LoadG3dModel(String Path){
+		G3dModelLoader loader = new G3dModelLoader(new JsonReader());
+		ModelData data = loader.loadModelData(Gdx.files.internal(Path));
+		Model ModTmp = new Model(data, new TextureProvider.FileTextureProvider());
+		return new ModelInstance(ModTmp);
+	}
+	
+	private ModelInstance LoadObjModel(String Path){
+		ObjLoader loader = new ObjLoader();
+		Model ModTmp = loader.loadModel(Gdx.files.internal(Path));
+		return new ModelInstance(ModTmp);
+	}
 
+  	private void ControlUpdate() {
+
+		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			mz = mz - 1f;
+		}
+		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+			mz = mz + 1f;
+		}
+		if (Gdx.input.isKeyPressed(Keys.UP)) {
+			mx = mx - 1f;
+		}
+		if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+			mx = mx + 1f;
+		}
+	}
+
+	private void ModelUpdate() {
+		modins.transform.setTranslation(mx, my, mz);
+	}
 	public void dispose() {
 		batch.dispose();
-		img.dispose();
 		mod.dispose();
+		board.dispose();
 		modbatch.dispose();
 	}
 
